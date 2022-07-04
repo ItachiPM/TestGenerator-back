9576f54bbc72e8b24a7458a9edb2e4d362b81f95
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-07-04 22:21:32 +0200

    changed db file

--- utils/db.ts
+++ utils/db.ts
@@ -1,10 +1,12 @@
 import {createPool} from "mysql2/promise";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname+'/.env' });
 
 export const pool = createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    user: 'root',
+    database: 'test_generator',
     namedPlaceholders: true,
     decimalNumbers: true,
 })
