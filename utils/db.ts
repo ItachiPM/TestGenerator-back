9846dc5ce5aa94a8fcc51f5d2ae4cc29a00ed7af
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-07-05 16:11:01 +0200

    db process env

--- utils/db.ts
+++ utils/db.ts
@@ -5,8 +5,9 @@ dotenv.config({ path: __dirname+'/.env' });
 
 export const pool = createPool({
     host: process.env.DB_HOST,
-    user: 'root',
-    database: 'test_generator',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     namedPlaceholders: true,
     decimalNumbers: true,
 })
