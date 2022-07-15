e7a9dbcf394ba44a3e72018e9a9ed8163faec609
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-07-15 15:12:17 +0200

    aded file to gitignore

--- congifData/configDataTemplate.ts
+++ congifData/configDataTemplate.ts
@@ -1,8 +1,8 @@
 export const config = {
     SESSION_SECRET: 'session secret',
     DB_HOST:'localhost',
-    DB_USER: 'root',
-    DB_PASSWORD: '',
-    DB_DATABASE: 'test_generator',
+    DB_USER: 'user',
+    DB_PASSWORD: 'password',
+    DB_DATABASE: 'database',
     CORS_ORIGIN: 'http://localhost:3000',
 }
